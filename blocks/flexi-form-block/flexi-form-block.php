a5172046e91c4cd8b1118a3cd0703380e3244399
odudename <odude.com@gmail.com>
2024-11-05 08:55:30 +0545

    form

--- blocks/flexi-form-block/flexi-form-block.php
+++ blocks/flexi-form-block/flexi-form-block.php
@@ -34,27 +34,74 @@ function create_block_flexi_form_block_block_init()
 add_action('init', 'create_block_flexi_form_block_block_init');
 
 
-function render_flexi_form_block($attributes)
+function render_flexi_form_block($args)
 {
 
+	$shortcode = "";
 	// Build the shortcode with all attributes
-	$shortcode = 'flexi-fffffff';
-	$shortcode .= '---';
+	if (isset($args['form_class'])) {
+
+		if (isset($args['enable_ajax']) && '1' == $args['enable_ajax']) {
+			$enable_ajax = "true";
+		} else {
+			$enable_ajax = "false";
+		}
+
+		if (isset($args['flexi_type']) && 'plain' == $args['flexi_type']) {
+			$flexi_type = '';
+		} else {
+			$flexi_type = 'type="' . $args['flexi_type'] . '"';
+		}
+
+		$shortcode .= '[flexi-form class="' . $args['form_class'] . '" title="' . $args['form_title'] . '" name="' . sanitize_title_with_dashes($args['form_title']) . '" ajax="' . $enable_ajax . '" ' . $flexi_type . ']';
+
+		$shortcode .= '[flexi-form-tag type="post_title" class="fl-input" title="' . $args['title_label'] . '" value="" placeholder="' . $args['title_placeholder'] . '" required="true"]';
+
+		if (isset($args['enable_category']) && '1' == $args['enable_category']) {
+			$shortcode .= '[flexi-form-tag type="category" title="' . $args['category_label'] . '" id="' . $args['cat'] . '"]';
+		}
+
+		if (isset($args['enable_tag']) && '1' == $args['enable_tag']) {
+			$shortcode .= '[flexi-form-tag type="tag" title="' . $args['tag_label'] . '"]';
+		}
+
+		if (isset($args['enable_desp']) && '1' == $args['enable_desp']) {
+			$shortcode .= '[flexi-form-tag type="article" class="fl-textarea" title="' . $args['desp_label'] . '" placeholder="' . $args['desp_placeholder'] . '"]';
+		}
+
+		if (isset($args['enable_file']) && '1' == $args['enable_file']) {
+			if (isset($args['enable_bulk_file']) && '1' == $args['enable_bulk_file']) {
+				$shortcode .= '[flexi-form-tag type="file_multiple" title="' . $args['file_label'] . '" class="flexi_drag_file" multiple="true" required="true"]';
+			} else {
+				$shortcode .= '[flexi-form-tag type="file" title="' . $args['file_label'] . '" required="true"]';
+			}
+		}
+
+		if (isset($args['enable_url']) && '1' == $args['enable_url']) {
+			$shortcode .= '[flexi-form-tag type="video_url" title="' . $args['url_label'] . '" value="" placeholder="eg. https://www.youtube.com/watch?v=uqyVWtWFQkY" required="true"]';
+		}
+
+		if (isset($args['enable_security']) && '1' == $args['enable_security']) {
+			$shortcode .= '[flexi-form-tag type="captcha" title="Security"]';
+		}
+
+		$shortcode .= '[flexi-form-tag type="submit" name="flexi_submit_button" value="' . $args['button_label'] . '"]';
+
+		$shortcode .= '[/flexi-form]';
+	}
 
 	// Output the shortcode with do_shortcode to render it properly
 	ob_start();
-	//echo $shortcode . "<hr>";
+	echo do_shortcode(wp_kses_post($shortcode));
 
 	// Display an admin preview if in REST context
 	if (defined('REST_REQUEST') && REST_REQUEST) {
-		echo "<small><div style='clear:both;border: 1px solid #999; background: #eee'>";
-		echo "<ul><li>Save & view frontend for actual output</li>";
-		echo "<li>This page only displays the shortcode which will be rendered.</li>";
-		echo "<li>Some settings may not work on specific layouts.</li></ul>";
-		//	echo '[' . esc_html($shortcode) . ']</div></small>';
-	} else {
-		//	echo do_shortcode('[' . $shortcode . ']');
-		echo "tet";
+		echo "<hr><div style='clear:both;border: 1px solid #999; background: #eee'>";
+		echo "<ul><li>Preview is for reference and may not view same.
+  <li>Below shortcode generated for this page</ul>";
+		echo '' . wp_kses_post($shortcode) . '</div>';
+		wp_enqueue_style('flexi_min', plugin_dir_url(__FILE__) . 'css/flexi-public-min.css', array(), FLEXI_VERSION, 'all');
+		wp_enqueue_style('flexi', plugin_dir_url(__FILE__) . 'css/flexi-public.css', array(), FLEXI_VERSION, 'all');
 	}
 
 	return ob_get_clean();
